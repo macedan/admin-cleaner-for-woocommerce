4fe3a2d9c3d2361a83acdba8abbed5a484c3fc26
Dan Mace <Daniel.mace@outlook.com>
2026-04-09 09:42:56 +0100

    Feature loading

---
+++ src/AdminCleaner.php
@@ -0,0 +1,36 @@
+<?php
+
+    namespace ACFW;
+
+    class AdminCleaner {
+
+        private static $instance = null;
+
+        private $features = [
+            Features\Disable_Analytics::class,
+        ];
+
+        /**
+         * Private constructor to prevent direct instantiation. Use get_instance() method instead.
+         */
+        private function __construct() {
+
+            // Loop over features + initialize them.
+            foreach ( $this->features as $feature_class ) {
+                $feature = new $feature_class();
+                if ( $feature->is_enabled() ) {
+                    $feature->boot();
+                }
+            }
+        }
+
+        /**
+         * Returns singleton instance of the AdminCleaner class.
+         */
+        public static function get_instance() {
+            if ( self::$instance === null ) {
+                self::$instance = new self();
+            }
+            return self::$instance;
+        }
+    }
