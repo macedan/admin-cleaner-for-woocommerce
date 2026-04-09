0d8a8c6515549640ce6f1adad1d966dc8013e137
Dan Mace <Daniel.mace@outlook.com>
2026-04-09 09:43:20 +0100

    Disable analytics feature base

---
+++ src/Features/Disable_Analytics.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace ACFW\Features; 
+
+    use ACFW\Utils\Abstract_Feature;
+
+    class Disable_Analytics extends Abstract_Feature {
+
+        public function boot() {
+            
+        }
+
+        public function disable_analytics_feature( ) {
+
+        }
+    }
