dc4a2c273c0afadde547d13a1e10b40947fab9ab
= <=>
2026-05-08 18:39:38 +0100

    Swap approach to filter rather than option setting

--- src/Features/Disable_AI_Bloat.php
+++ src/Features/Disable_AI_Bloat.php
@@ -13,17 +13,11 @@
 
         public function boot() {
 
-            // Check if Yoast SEO is active before proceeding.
-            if ( ! class_exists( 'WPSEO_Options' ) ) {
-                return;
-            }
-
-            // Check status of Yoast AI in the database, and if it is disabled, return early.
-            if ( \WPSEO_Options::get_option( 'enable_ai_generator', false ) === false ) {
-                return;
-            }
-
-            // Manually set the SEO AI content analysis option to false, as it is enabled by default.
-            \WPSEO_Options::save_option( 'wpseo', 'enable_ai_generator', false );
+            // Hook into yoast options to disable AI features in-flight.
+            add_filter( 'option_wpseo', function( $options ) {
+                $options['enable_ai_generator'] = false;
+                return $options;
+            });
         }
     }
+    
