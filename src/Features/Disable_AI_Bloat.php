abf79429de04295a2d49e438196c9576b2e294d3
= <=>
2026-05-08 18:51:03 +0100

    Disable jetpack AI

--- src/Features/Disable_AI_Bloat.php
+++ src/Features/Disable_AI_Bloat.php
@@ -16,6 +16,9 @@
             // Disable Core WP AI features.
             add_filter( 'wp_supports_ai', '__return_false' );
 
+            // Disable Jetpacj AI features.
+            add_filter( 'jetpack_ai_enabled', '__return_false' );
+
             // Hook into yoast options to disable AI features in-flight.
             add_filter( 'option_wpseo', function( $options ) {
                 $options['enable_ai_generator'] = false;
