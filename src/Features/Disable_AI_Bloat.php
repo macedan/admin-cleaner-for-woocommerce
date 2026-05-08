21ea5ab5951e81d8f04f0493478343eafdba4213
= <=>
2026-05-08 18:53:20 +0100

    typo

--- src/Features/Disable_AI_Bloat.php
+++ src/Features/Disable_AI_Bloat.php
@@ -16,7 +16,7 @@
             // Disable Core WP AI features.
             add_filter( 'wp_supports_ai', '__return_false' );
 
-            // Disable Jetpacj AI features.
+            // Disable Jetpack AI features.
             add_filter( 'jetpack_ai_enabled', '__return_false' );
 
             // Hook into yoast options to disable AI features in-flight.
